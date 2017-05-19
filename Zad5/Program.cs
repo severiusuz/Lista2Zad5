25f884bd3ca77e8e4cd8b997a014fb600bef966f
severiusuz <severiusuz@gmail.com>
2017-05-19 14:06:16 +0200

    init

---
+++ Zad5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+namespace Zad5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rand = new Random();
+            int[] values = new int[100];
+            for (int i = 0; i < 100; i++)
+                values[i] = rand.Next(1000);
+            var searchEngine = new SearchValue();
+
+            int maxValue = int.MaxValue;
+            int minValue = int.MinValue;
+
+            var maxThread = new Thread(() => maxValue = searchEngine.SearchForMax(values));
+            var minThread = new Thread(() => minValue = searchEngine.SearchForMin(values));
+
+            maxThread.Start();
+            minThread.Start();
+            maxThread.Join();
+            minThread.Join();
+
+            Console.WriteLine("Max: " + maxValue);
+            Console.WriteLine("Min: " + minValue);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Zad5/SearchValue.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+
+namespace Zad5
+{
+    class SearchValue
+    {
+        public int SearchForMax(int[] values)
+        {
+            return values.Max();
+        }
+
+        public int SearchForMin(int[] values)
+        {
+            return values.Min();
+        }
+    }
+}
